0900c0c5aae34f9d0a32cce4e305bb8b8c502b14
Maksym Tymtsiv <mtymtsiv@gmail.com>
2017-03-30 12:56:04 -0400

    Initial commit

--- jf/settings.py
+++ jf/settings.py
@@ -13,6 +13,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
+SECRET_KEY = os.environ['SECRET_KEY']
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
